2364f4c5d68b9d6cf382d67083991d464ea30031
Maxwell Carter <max4carter@gmail.com>
2018-02-14 22:43:57 -0500

    Scaffolding

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    log: 'sleepyzzz-analyzer.log',
+    database: {
+        url: "https://sleepyzzz-38a10.firebaseio.com/",
+        serviceAccount: "../svc-sleepyzzz.json",
+        node: "sleepyzzz-dev"
+    }
+}
