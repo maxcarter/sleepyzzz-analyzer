50be11c1e6597e355786faf053769064aa1050af
Maxwell Carter <max4carter@gmail.com>
2018-02-16 14:44:01 -0500

    Implemented heartrate listener anomaly detector

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 module.exports = {
     log: 'sleepyzzz-analyzer.log',
+    buffer: 30000,
     database: {
         url: "https://sleepyzzz-38a10.firebaseio.com/",
         serviceAccount: "../svc-sleepyzzz.json",
         node: "sleepyzzz-dev"
+    },
+    anomalies: {
+      heartrate: {
+        upperLimit: 160,
+        lowerLimit: 90
+      }
     }
 }
--- index.js
+++ index.js
@@ -9,46 +9,84 @@ log.add(log.transports.File, {
 log.info('Starting SleepyZzz Analyzer')
 
 ctrls.database.listen('devices', (snapshot) => {
+    log.info(`Setting up analyzer for device [${snapshot.key}]`)
     let device = snapshot.val()
-    let babyKey = device.baby
-    let userKey = device.user
-    ctrls.database.listenChild('heartrate', babyKey, (snap) => {
-        let data = snap.val()
-        let anomaly = (data.bpm > 160) || (data.bpm < 90)
+    let disconnected = false
+    let anomaliesBuffer = {
+        heartrate: {
+            fast: [],
+            slow: []
+        }
+    }
 
+    ctrls.database.listen(`heartrate/${device.baby}`, (snap) => {
+        let data = snap.val()
+        if (data.bpm <= 0) {
+            disconnected = true
+            return
+        }
+        disconnected = false
+        let anomaly =
+            data.bpm > config.anomalies.heartrate.upperLimit ||
+            data.bpm < config.anomalies.heartrate.lowerLimit
 
         if (!anomaly) {
             return
         }
 
-        ctrls.database.read('users', userKey).then((user) => {
-            let message = {}
-            let babyName = user.babies[babyKey]
-            log.info('Notifying user mobile devices')
+        let type = ''
+        if (data.bpm > config.anomalies.heartrate.upperLimit) {
+            type = 'fast'
+        } else if (data.bpm < config.anomalies.heartrate.lowerLimit) {
+            type = 'slow'
+        }
+        let now = Date.now()
+        let latestNotification = now
+        if (anomaliesBuffer.heartrate[type].length > 0) {
+            latestNotification = anomaliesBuffer.heartrate[type][0]
+        }
 
-            if (data.bpm > 160) {
-                message.title = babyName + "'s heartrate is too fast!"
-            } else if (data.bpm < 90) {
-                message.title = babyName + "'s heartrate is too slow!"
-            }
+        let timelapse = now - latestNotification
+        if (timelapse < config.buffer && timelapse != 0) {
+            return
+        }
+        anomaliesBuffer.heartrate[type].push(now)
 
-            message.body = babyName + "'s heartrate is currently: " + data.bpm + ' BPM.'
+        ctrls.database.read('users', device.user)
+            .then((user) => {
+                let babyName = user.babies[device.baby]
 
-            for (let mKey in user.mobileDevices) {
-                ctrls.notifications.send(user.mobileDevices[mKey], message.title, message.body)
-            }
-        }).catch((error) => {
-            log.error(`Could not find user [${userKey}]`)
-        })
-    })
+                let message = {
+                    title: `${babyName}'s heartrate is too ${type}!`,
+                    body: `${babyName}'s heartrate is currently: ${data.bpm} BPM.`
+                }
 
-    ctrls.database.listenChild('movement', babyKey, (snap) => {
-        let data = snap.val()
-        log.info(data)
-    })
+                let notifications = []
+                for (let k in user.mobileDevices) {
+                    notifications.push(ctrls.notifications.send(user.mobileDevices[k], message.title, message.body))
+                }
+                Promise.all(notifications)
+                    .then((results) => {
+                        setTimeout(() => {
+                            anomaliesBuffer.heartrate[type].pop()
+                        }, config.buffer)
+                    })
+                    .catch((error) => {
+                        log.error(error)
+                        anomaliesBuffer.heartrate[type].pop()
+                    })
+                ctrls.database.insert(`anomalies/${device.baby}/heartrate`, {
+                    timestamp: data.timestamp,
+                    message: message.title,
+                    details: message.body
+                })
 
-    ctrls.database.listenChild('temperature', babyKey, (snap) => {
-        let data = snap.val()
-        log.info(data)
+            })
+            .catch((error) => {
+                log.error(error)
+                anomaliesBuffer.heartrate[type].pop()
+            })
+    }, {
+        limit: 1
     })
 })
