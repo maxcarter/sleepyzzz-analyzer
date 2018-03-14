e29c4f5d3592086a51d6ba6f3d756d6babd4a280
Maxwell Carter <max4carter@gmail.com>
2018-03-14 02:01:53 -0400

    Moved anomaly detection code for devices into anomalies folder

--- anomalies/index.js
+++ anomalies/index.js
@@ -1,5 +1,16 @@
-module.exports = (device, configuration) => {
-    require('./heartrate')(device, configuration)
-    require('./temperature')(device, configuration)
-    require('./movement')(device, configuration)
+const log = require('winston')
+var config = require('../config')
+var ctrls = require('../controllers')
+
+module.exports = () => {
+    ctrls.database.listen('devices', (snapshot) => {
+        log.info(`Setting up analyzer for device [${snapshot.key}]`)
+        let device = snapshot.val()
+        let configuration = {
+            disconnected: false
+        }
+        require('./heartrate')(device, configuration)
+        require('./temperature')(device, configuration)
+        require('./movement')(device, configuration)
+    })
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const log = require('winston')
 var config = require('./config')
-var ctrls = require('./controllers')
 
 log.add(log.transports.File, {
     filename: config.log
@@ -8,11 +7,4 @@ log.add(log.transports.File, {
 
 log.info('Starting SleepyZzz Analyzer')
 
-ctrls.database.listen('devices', (snapshot) => {
-    log.info(`Setting up analyzer for device [${snapshot.key}]`)
-    let device = snapshot.val()
-    let configuration = {
-      disconnected: false
-    }
-    require('./anomalies')(device, configuration)
-})
+require('./anomalies')()
